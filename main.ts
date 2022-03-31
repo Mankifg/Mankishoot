6d438669dcd34bf1f54ebb42797260ce815f048a
Mankifg <83959255+Mankifg@users.noreply.github.com>
2022-03-31 21:41:40 +0200

    improve start

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 input.onButtonPressed(Button.A, function () {
     p1.change(LedSpriteProperty.X, -1)
-    radio.sendValue("move", p1.get(LedSpriteProperty.X))
     p1.ifOnEdgeBounce()
     basic.pause(200)
 })
@@ -11,7 +10,6 @@ input.onButtonPressed(Button.AB, function () {
         radio.sendValue("bset", p1.get(LedSpriteProperty.X))
         for (let index = 0; index < 4; index++) {
             bullet.change(LedSpriteProperty.Y, -1)
-            radio.sendValue("bmove", -1)
             basic.pause(250)
         }
         bullet.delete()
@@ -25,7 +23,6 @@ radio.onReceivedString(function (receivedString) {
 })
 input.onButtonPressed(Button.B, function () {
     p1.change(LedSpriteProperty.X, 1)
-    radio.sendValue("move", p1.get(LedSpriteProperty.X))
     p1.ifOnEdgeBounce()
     basic.pause(200)
 })
@@ -37,21 +34,20 @@ radio.onReceivedValue(function (name, value) {
         ebullet = game.createSprite(value, 0)
     }
     if (name == "bmove") {
-        ebullet.change(LedSpriteProperty.Y, 1)
-        if (ebullet.isTouching(p1)) {
-            radio.sendString("end")
-            basic.showIcon(IconNames.No)
-        }
+        bullet.set(LedSpriteProperty.X, value)
     }
     if (name == "b") {
         ebullet.delete()
     }
 })
-let ebullet: game.LedSprite = null
-let bullet: game.LedSprite = null
 let timer = 0
 let p2: game.LedSprite = null
 let p1: game.LedSprite = null
+let ebullet: game.LedSprite = null
+let bullet: game.LedSprite = null
+basic.clearScreen()
+bullet.delete()
+ebullet.delete()
 radio.setGroup(196)
 p1 = game.createSprite(2, 4)
 p2 = game.createSprite(2, 0)
@@ -59,4 +55,8 @@ timer = 0
 basic.forever(function () {
     basic.pause(1000)
     timer += 1000
+})
+basic.forever(function () {
+    radio.sendValue("move", p1.get(LedSpriteProperty.X))
+    radio.sendValue("bmove", bullet.get(LedSpriteProperty.X))
 })
