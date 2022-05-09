0c7b822b3cb6b78fb2e6cb309138f7d0e9a261c4
Mankifg <83959255+Mankifg@users.noreply.github.com>
2022-05-09 12:33:10 +0200

    fixed shot display

--- main.ts
+++ main.ts
@@ -1,28 +1,29 @@
-function shot (x: number) {
-    if (timer > 2000) {
-        timer = 0
-        bullet = game.createSprite(x, 5)
-        radio.sendValue("bset", x)
-        for (let index = 0; index < 4; index++) {
-            bullet.change(LedSpriteProperty.Y, -1)
-            radio.sendValue("bmove", bullet.get(LedSpriteProperty.X))
-            basic.pause(250)
-        }
-        bullet.delete()
-        radio.sendValue("b", 0)
+function simulate (x: number, y: number) {
+    ebullet = game.createSprite(x, y)
+    for (let index = 0; index < 4; index++) {
+        ebullet.change(LedSpriteProperty.Y, 1)
+        basic.pause(250)
     }
+    ebullet.delete()
 }
 input.onButtonPressed(Button.A, function () {
     p1.change(LedSpriteProperty.X, -1)
     p1.ifOnEdgeBounce()
     basic.pause(200)
 })
+function self_simulate (x: number, y: number) {
+    bullet = game.createSprite(x, y)
+    for (let index = 0; index < 4; index++) {
+        bullet.change(LedSpriteProperty.Y, -1)
+        basic.pause(250)
+    }
+    bullet.delete()
+}
 input.onButtonPressed(Button.AB, function () {
-    shot(p1.get(LedSpriteProperty.X))
-})
-radio.onReceivedString(function (receivedString) {
-    if (receivedString == "end") {
-        basic.showIcon(IconNames.Yes)
+    if (timer > 2000) {
+        timer = 0
+        radio.sendValue("bset", p1.get(LedSpriteProperty.X))
+        self_simulate(p1.get(LedSpriteProperty.X), 4)
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -35,17 +36,11 @@ radio.onReceivedValue(function (name, value) {
         p2.set(LedSpriteProperty.X, value)
     }
     if (name == "bset") {
-        ebullet = game.createSprite(value, 0)
-    }
-    if (name == "bmove") {
-        bullet.set(LedSpriteProperty.X, value)
-    }
-    if (name == "b") {
-        ebullet.delete()
+        simulate(value, 0)
     }
 })
-let ebullet: game.LedSprite = null
 let bullet: game.LedSprite = null
+let ebullet: game.LedSprite = null
 let timer = 0
 let p2: game.LedSprite = null
 let p1: game.LedSprite = null
@@ -55,9 +50,9 @@ p2 = game.createSprite(2, 0)
 timer = 0
 radio.setGroup(196)
 basic.forever(function () {
-    radio.sendValue("move", p1.get(LedSpriteProperty.X))
-})
-basic.forever(function () {
     basic.pause(1000)
     timer += 1000
+})
+basic.forever(function () {
+    radio.sendValue("move", p1.get(LedSpriteProperty.X))
 })
